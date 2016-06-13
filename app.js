3903997838e1151562d12c5a96812064ab56cb81
Mark <marck1391@gmail.com>
2016-06-13 01:15:33 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require('./config')
+var port = process.env.PORT||3000;
+var express = require('express');
+var redis = require('redis')
+
+var pub = redis.createClient(redisurl)
+var sub = redis.createClient(redisurl)
+
+var app = express();
+
+app.set('view engine', 'ejs')
+app.get('/', function(req, res){
+  res.render('index', {port: port})
+})
+
+var server = app.listen(port, function(err){
+  console.log('Listening on localhost:%d', server.address().port)
+})
+
+var io = require('socket.io')(server);
+sub.subscribe('chat')
+io.on('connection', function(socket){
+  socket.on('message', function(message){
+    pub.publish('chat', message)
+  })
+
+  sub.on('message', function(channel, message){
+    socket.emit(channel, {message: message, port: port})
+  })
+})
